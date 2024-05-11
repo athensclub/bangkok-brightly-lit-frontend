a226b705e35f7e5e3c34ec167739c0c76ed7843e
athensclub <athens_club@hotmail.com>
2024-05-11 15:56:00 +0700

    edit: display main dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen">
-      {/* <Dashboard></Dashboard> */}
-      <Dashboard2 ></Dashboard2>
+      <Dashboard></Dashboard>
+      {/* <Dashboard2 ></Dashboard2> */}
     </main>
   );
 }
