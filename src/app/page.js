e4288731d0b181c3079ffb96550a258901ab2a4d
athensclub <athens_club@hotmail.com>
2024-05-11 15:55:19 +0700

    add: 2 more ai generated components

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 "use client";
 import { Dashboard } from "@/components/component/dashboard";
+import { Dashboard2 } from "@/components/component/dashboard2";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen">
-      <Dashboard></Dashboard>
+      {/* <Dashboard></Dashboard> */}
+      <Dashboard2 ></Dashboard2>
     </main>
   );
 }
